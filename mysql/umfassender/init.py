e3d4798b096cfe57ece2352a08ec15806634ec9a
wolfgang-wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2014-12-04 17:35:42 +0100

    Veraltete Version von init.py ersetzt

--- mysql/umfassender/init.py
+++ mysql/umfassender/init.py
@@ -1,14 +1,7 @@
-from model import ModelUtility, RoleEntity, RoleVersion
-from datetime import datetime
+from Role import Role
+from RoleRepository import RoleRepository
 
-db = ModelUtility.db
-ModelUtility.Base.metadata.create_all(db)
-session = ModelUtility.SessionFactory()
-
-# Beispielrolle anlegen
-r = RoleEntity(rolename="Beispielrolle")
-r.create_date = datetime.now()
-r.deleted = False
-
-session.add(r)
-session.commit()
+repo = RoleRepository()
+r1 = Role(rolename="Beispielrolle")
+r1.permissions = ['Berechtigung 1']
+role = repo.create_role(r1)
